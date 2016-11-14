ac5ba5e921bcde2dc517b1ff99647391eb731cce
Shane Jarman <sjarman91@gmail.com>
2016-11-14 01:09:41 -0500

    map sort of working

--- app/components/location.js
+++ app/components/location.js
@@ -19,12 +19,13 @@ class Location extends Component {
             </div>
           </div>
           <div className="row">
-            <div className="col-md-2">
-              <ChartComponent />
+            <div className="col-md-1">
+
             </div>
-            <div className="col-md-8">
+            <div className="col-md-10">
+            <ChartComponent />
             </div>
-            <div className="col-md-2">
+            <div className="col-md-1">
             </div>
           </div>
         </div>
@@ -41,28 +42,89 @@ export default Location
 
 
 function ChartComponent (props) {
-  var config = {
-    chart: {
-      spacingBottom: 20
-    },
-    title : {
-      text : 'basic demo'
-    },
 
-    legend: {
-      enabled: true
-    },
+    const config = {
 
-    plotOptions: {
-      map: {
-        mapData: Highcharts.maps['custom/world'],
-      }
+        title: {
+            text: 'Highmaps basic lat/lon demo'
+        },
+
+        mapNavigation: {
+            enabled: true
+        },
+
+        tooltip: {
+            headerFormat: '',
+            pointFormat: '<b>{point.name}</b><br>Lat: {point.lat}, Lon: {point.lon}'
+        },
+
+        series: [{
+            // Use the gb-all map with no data as a basemap
+
+            mapData: Highcharts.maps['custom/world'],
+            borderColor: '#A0A0A0',
+            nullColor: 'rgba(200, 200, 200, 0.3)',
+            showInLegend: false
+        }, {
+            name: 'Separators',
+            type: 'mapline',
+            data: Highcharts.geojson(Highcharts.maps['custom/world'], 'mappoint'),
+            color: '#707070',
+            showInLegend: false,
+            enableMouseTracking: false
+        }, {
+            // Specify points using lat/lon
+            type: 'mappoint',
+            name: 'Tweets',
+            color: Highcharts.getOptions().colors[1],
+            data: [{
+                name: 'London',
+                lat: 51.507222,
+                lon: -0.1275
+            }, {
+                name: 'Birmingham',
+                lat: 52.483056,
+                lon: -1.893611
+            }, {
+                name: 'Leeds',
+                lat: 53.799722,
+                lon: -1.549167
+            }, {
+                name: 'Glasgow',
+                lat: 55.858,
+                lon: -4.259
+            }, {
+                name: 'Sheffield',
+                lat: 53.383611,
+                lon: -1.466944
+            }, {
+                name: 'Liverpool',
+                lat: 53.4,
+                lon: -3
+            }, {
+                name: 'Bristol',
+                lat: 51.45,
+                lon: -2.583333
+            }, {
+                name: 'Belfast',
+                lat: 54.597,
+                lon: -5.93
+            }, {
+                name: 'Lerwick',
+                lat: 60.155,
+                lon: -1.145,
+                dataLabels: {
+                    align: 'left',
+                    x: 5,
+                    verticalAlign: 'middle'
+                }
+            }]
+        }]
     }
-  };
-    console.log(Highcharts.maps['custom/world'])
+
     return (
         <div>
-            {React.createElement(ReactHighmaps, { config: config })}
+            {React.createElement(ReactHighmaps, {config: config})}
         </div>
     )
 }
